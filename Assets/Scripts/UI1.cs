8bc93231010fd3477aae7547bfe7fed4306c994b
shihailong <shihailong@blackjack-inc.com>
2021-03-05 17:44:22 +0800

    1111111

---
+++ Assets/Scripts/UI1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI1 : MonoBehaviour
+{
+    public InputField IP;
+
+    public InputField Port;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IP.onValueChanged.AddListener(newText =>
+        {
+            Debug.Log($"new value = {newText}");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
